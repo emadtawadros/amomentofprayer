a191efd635bf940218ed60b260597b9cf5015a0b
emadtawadros <emad.tawadros@gmail.com>
2015-03-12 13:37:47 -0700

    Adding the createentitycomponent

--- js/components.js
+++ js/components.js
@@ -1,1 +1,18 @@
-
+Hull.component('createentityform', {
+  templates: ['createentityform'],
+  actions: {
+    createentity: function(){
+        var component = this;
+        var newEntityName = this.$el.find('#newEntityField').val();
+        if(newEntityName)
+        {
+          component.api('/app/entities', 'post',{
+            "uid": null,
+            "name": newEntityName
+          }).then(function(response) {
+            console.log(response);
+          });
+        }
+    }
+  }
+});
