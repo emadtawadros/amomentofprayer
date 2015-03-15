65d7c6564928666d44d07eda1bd8a2cd211dac7a
emadtawadros <emad.tawadros@gmail.com>
2015-03-15 12:28:05 -0700

    Update components.js

--- js/components.js
+++ js/components.js
@@ -75,15 +75,17 @@ Hull.component('quotes', {
   actions: {
   },
   rotateQuotes: function (component) {
+    var fadeInFunction = component.fadeIn;
     var currentActiveDiv = component.$el.find('[data-isActive="true"]');
     var currentInactiveDiv = component.$el.find('[data-isActive="false"]');
     
-    currentActiveDiv.fadeOut(500, function(){
-      currentActiveDiv.attr("data-isActive", "false");
-      currentInactiveDiv.fadeIn(500, function() {
-        currentInactiveDiv.attr("data-isActive", "true");
-        component.rotateQuotes(component);
-      });
+    currentActiveDiv.fadeOut(500).delay(5000).fadeInFunction(currentActiveDiv, currentInactiveDiv, component);
+  },
+  fadeIn: function(currentActiveDiv, currentInactiveDiv, component) {
+    currentActiveDiv.attr("data-isActive", "false");
+    currentInactiveDiv.fadeIn(500, function() {
+      currentInactiveDiv.attr("data-isActive", "true");
+      component.rotateQuotes(component);
     });
   }
 });
