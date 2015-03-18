cd39e45d6ffee9e3e179c783a3d24f8eff51b7a7
emadtawadros <emad.tawadros@gmail.com>
2015-03-17 21:43:50 -0700

    Update components.js

--- js/components.js
+++ js/components.js
@@ -50,7 +50,6 @@ Hull.component('quotes', {
    quotes: function() {
      return this.api('5504676b91e0cb0be00014cd/conversations', 'get', {
        'wrapped': true,
-       'per_page': 2,
        'page': this.options.page
      });
    }
