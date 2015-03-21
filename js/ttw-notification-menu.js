17597337ef7e0b21380dadb541ad0afebf230558
emadtawadros <emad.tawadros@gmail.com>
2015-03-21 09:47:27 -0700

    bug

--- js/ttw-notification-menu.js
+++ js/ttw-notification-menu.js
@@ -416,7 +416,7 @@
                 self.$wrapper.remove();
                 menuItems[self.settings.category].notificationList = false;
                 notificationList = false;
-                runCallback(notificationMenuCloseCallback, this);
+                runCallback(options.notificationMenuCloseCallback, this);
             });
         };
 
