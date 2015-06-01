c045c4dd6acbdfe90428e615a918f3879ed49a14
emadtawadros <emad.tawadros@gmail.com>
2015-06-01 16:56:21 -0700

    adding more options

--- js/components.js
+++ js/components.js
@@ -176,13 +176,32 @@ Hull.component('submitprayerform', {
           }).then(function(response) {
             console.log(response);
             var n = noty({
-              text: 'NOTY - a jquery notification library!',
+              layout: 'top',
+              theme: 'defaultTheme', // or 'relax'
+              type: 'alert',
+              text: '', // can be html or string
+              dismissQueue: true, // If you want to use queue feature set this true
+              template: '<div class="noty_message"><span class="noty_text"></span><div class="noty_close"></div></div>',
               animation: {
-                open: 'animated bounceInLeft', // Animate.css class names
-                close: 'animated bounceOutLeft', // Animate.css class names
-                easing: 'swing', // unavailable - no need
-                speed: 500 // unavailable - no need
-                }
+                  open: {height: 'toggle'}, // or Animate.css class names like: 'animated bounceInLeft'
+                  close: {height: 'toggle'}, // or Animate.css class names like: 'animated bounceOutLeft'
+                  easing: 'swing',
+                  speed: 500 // opening & closing animation speed
+              },
+              timeout: false, // delay for closing event. Set false for sticky notifications
+              force: false, // adds notification to the beginning of queue when set to true
+              modal: false,
+              maxVisible: 5, // you can set max visible notification for dismissQueue true option,
+              killer: false, // for close all notifications before show
+              closeWith: ['click'], // ['click', 'button', 'hover', 'backdrop'] // backdrop click will close all notifications
+              callback: {
+                  onShow: function() {},
+                  afterShow: function() {},
+                  onClose: function() {},
+                  afterClose: function() {},
+                  onCloseClick: function() {},
+              },
+              buttons: false // an array of buttons
             });
             
             component.$el.find('#submitPrayerForm').fadeOut(500, function(){
