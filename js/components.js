2578a6f9ac9a09cacf53f90692b8e83717f6f60e
emadtawadros <emad.tawadros@gmail.com>
2015-06-02 11:15:51 -0700

    invoking other tip and adding warning

--- js/components.js
+++ js/components.js
@@ -152,6 +152,7 @@ Hull.component('submitprayerform', {
       this.$el.find('#enableSubmitForm').fadeOut(500, function(){
         component.$el.find('#submitPrayerForm').fadeIn(500, function(){
           component.$el.find('#prayerOwnerTip').tooltipster();
+          component.$el.find('#prayerTextTip').tooltipster();
         });
       });
     },
@@ -216,6 +217,35 @@ Hull.component('submitprayerform', {
               component.$el.find('#enableSubmitForm').fadeIn(500);
             });
           });
+        } else {
+              var n = noty({
+              layout: 'top',
+              theme: 'relax', // or 'defaultTheme'
+              type: 'warning',
+              text: "Oops! Looks like you've missed a field!", // can be html or string
+              dismissQueue: false, // If you want to use queue feature set this true
+              template: '<div class="noty_message"><span class="noty_text"></span><div class="noty_close"></div></div>',
+              animation: {
+                  open: {height: 'toggle'}, // or Animate.css class names like: 'animated bounceInLeft'
+                  close: {height: 'toggle'}, // or Animate.css class names like: 'animated bounceOutLeft'
+                  easing: 'swing',
+                  speed: 500 // opening & closing animation speed
+              },
+              timeout: false, // delay for closing event. Set false for sticky notifications
+              force: false, // adds notification to the beginning of queue when set to true
+              modal: false,
+              maxVisible: 5, // you can set max visible notification for dismissQueue true option,
+              killer: false, // for close all notifications before show
+              closeWith: ['click'], // ['click', 'button', 'hover', 'backdrop'] // backdrop click will close all notifications
+              callback: {
+                  onShow: function() {},
+                  afterShow: function() {},
+                  onClose: function() {},
+                  afterClose: function() {},
+                  onCloseClick: function() {},
+              },
+              buttons: false // an array of buttons
+            });
         }
     }
   }
